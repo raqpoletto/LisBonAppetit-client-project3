e506b1f2419978dce819fcfdc3ea34cb2c2d85fc
raqpoletto <polettoraq@gmail.com>
2022-09-02 08:47:18 +0100

    running

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -5,12 +5,73 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Button from "react-bootstrap/Button";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { ListGroup, ListGroupItem } from "react-bootstrap";
 
 function MyNavbar() {
   const { loggedIn, user, logout } = useContext(AuthContext);
 
   return (
-    <Navbar bg="transparent">
+    <Navbar bg="transparent" expand="lg">
+      <Container>
+        <Navbar.Brand as={Link} to={"/"}>
+          <img
+            src="https://res.cloudinary.com/poletto/image/upload/v1662057879/Restaurants/Captura_de_Tela_2022-09-01_a%CC%80s_18.33.45_yhopnk.png"
+            style={{ marginLeft: "10px" }}
+            width="300px"
+            alt="logo"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto user-logged">
+            {loggedIn && (
+              <>
+                <Nav.Link href="/restaurants">All Restaurants</Nav.Link>
+                <Nav.Link href="/restaurants/create">
+                  Add New Restaurant
+                </Nav.Link>
+                <Nav.Link href="/restaurant/edit/:restaurantId">
+                  Update Restaurant
+                </Nav.Link>
+                <NavDropdown title={"Hello, Raquel"} id="basic-nav-dropdown">
+                  <NavDropdown.Item href="/profile">Profile</NavDropdown.Item>
+
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="Logout">
+                    <Nav.Link
+                      variant="outline-danger"
+                      href="/profile"
+                      onClick={() => logout()}
+                    >
+                      Logout
+                    </Nav.Link>
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+
+        {!loggedIn && (
+          <div className="col-md-3 text-end">
+            <Link to="/signup">
+              <Button variant="outline-danger">Sign Up</Button>
+            </Link>
+            <Link to="/login">
+              <Button variant="outline-danger">Login</Button>
+            </Link>
+          </div>
+        )}
+      </Container>
+    </Navbar>
+  );
+}
+
+export default MyNavbar;
+
+{
+  /* <Navbar bg="transparent">
       <Container fluid>
         <Navbar.Brand as={Link} to={"/"}>
           <img
@@ -62,8 +123,5 @@ function MyNavbar() {
           )}
         </Nav>
       </Container>
-    </Navbar>
-  );
+    </Navbar> */
 }
-
-export default MyNavbar;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,74 @@ import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 
 function HomePage() {
-  return;
+  return (
+    <section className="container-fluid">
+      <div className="row bg-dark text-white">
+        <div className="col-lg-7 p-0">
+          <Carousel className="carrosel-ph">
+            <Carousel.Item interval={4000}>
+              <img
+                className="d-block w-100 "
+                src="https://res.cloudinary.com/poletto/image/upload/v1662063256/Restaurants/pexels-vincent-ma-janssen-1310777_giejjb.jpg"
+                alt="First slide"
+              />
+              <Carousel.Caption>
+                <h1>Looking for an affordable dinner in Lisbon?</h1>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item interval={4000}>
+              <img
+                className="d-block w-100"
+                src="https://res.cloudinary.com/poletto/image/upload/v1662063260/Restaurants/pexels-quang-nguyen-vinh-2159065_ylkuqz.jpg"
+                alt="Second slide"
+              />
+              <Carousel.Caption>
+                <h1>Authentic and under the radar restaurants</h1>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item interval={4000}>
+              <img
+                className="d-block w-100"
+                src="https://res.cloudinary.com/poletto/image/upload/v1661964411/Restaurants/mdlmqfkeahla3abyks7b.jpg"
+                alt="Third slide"
+              />
+              <Carousel.Caption>
+                <h1>Find meals under €15</h1>
+              </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item interval={4000}>
+              <img
+                className="d-block w-100"
+                src="https://res.cloudinary.com/poletto/image/upload/v1662063261/Restaurants/pexels-creative-vix-370984_sbe0b4.jpg"
+                alt="Third slide"
+              />
+              <Carousel.Caption>
+                <h1>Discover the city!</h1>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </div>
+
+        <div className="col-lg-5 p-4 align-self-center">
+          <h1 className="display-4">Find your hotspot in Lisbon!</h1>
+          <p class="lead">
+            Find your hotspot in Lisbon! We believe that delicious and genuine
+            food doesn't have to be absurdly expensive. This selection was
+            visited and revisited, selected not only for the attractive price
+            but also because they are cozy restaurants, with truly good meals
+            and with the Lisbon charm. You can also join and add your favorite
+            places to our list. Have fun and thank you for visiting this little
+            corner of a former chef's mind.
+          </p>
+        </div>
+      </div>
+
+      <div id="footer">
+        <h2>LisBon Appétit &copy; by Raquel Poletto </h2>
+      </div>
+    </section>
+  );
 }
 
 export default HomePage;
